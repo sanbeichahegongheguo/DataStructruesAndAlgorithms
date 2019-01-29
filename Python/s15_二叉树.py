a82e4dc5c4f7ea645ac86028dd2da1b359ad94db
Volcano_ash <32977199+sanbeichahegongheguo@users.noreply.github.com>
2019-01-29 14:27:22 +0800

    增加返回索引值的代码

---
+++ Python/s15_二叉树.py
@@ -0,0 +1,10 @@
+class Node(object):
+    def __init__(self, item):
+        self.elem = item
+        self.lchild = None
+        self.rchild = None
+
+class Tree(object):
+    def __init__(self):
+        self.root = None
+        
